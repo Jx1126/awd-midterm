fe746c4cdcf6bc841d03cdc8bd014fabcee33740
Jx1126 <147470595+Jx1126@users.noreply.github.com>
2024-12-16 16:21:08 +0800

    Imported CSV and created base model for the database

--- mrt_station_project/mrt_stations/models.py
+++ mrt_station_project/mrt_stations/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class MRTStation(models.Model):
+  name = models.CharField(max_length=100)
+  stop_id = models.CharField(max_length=10, unque = True)
+  line = models.CharField(max_length=10)
+  no = models.IntegerField()
+  lng = models.FloatField()
+  lat = models.FloatField()
+  subzone_no = models.IntegerField()
+  subzone_n = models.CharField(max_length=100)
+  subzone_c = models.CharField(max_length=20)
+  pln_area_n = models.CharField(max_length=100)
+  pln_area_c = models.CharField(max_length=10)
+  region_n = models.CharField(max_length=100)
+  region_c = models.CharField(max_length=10)
+
+  def __str__(self):
+    return self.name

e61c7d6c84da548af14b7fd0860444a4fd7a3b40
Jx1126 <147470595+Jx1126@users.noreply.github.com>
2024-12-23 20:38:58 +0800

    Reimported serializers that got got omitted from previous commit. Added api to url path for rest endpoints. Implemented a simple rest endpoint to return all the movies and its detail.

--- movies_2023/api/serializers.py
+++ movies_2023/api/serializers.py
@@ -1,3 +1,25 @@
 from rest_framework import serializers
 from .models import Movie, Genre, Director, Star
 
+class GenreSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Genre
+    fields = '__all__'
+
+class DirectorSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Director
+    fields = '__all__'
+
+class StarSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Star
+    fields = '__all__'
+
+class MovieSerializer(serializers.ModelSerializer):
+  genre = GenreSerializer(many=True)
+  director = DirectorSerializer(many=True)
+  stars = StarSerializer(many=True) 
+  class Meta:
+    model = Movie
+    fields = '__all__'
--- movies_2023/api/views.py
+++ movies_2023/api/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+from .models import Movie, Genre, Director, Star
+from .serializers import MovieSerializer, GenreSerializer, DirectorSerializer, StarSerializer
 
 @api_view(['GET'])
-def getData(request):
-  person={'name':'John','age':30}
-  return Response(person)
-
+def getAllMovies(request):
+    movies = Movie.objects.all()
+    serializer = MovieSerializer(movies, many=True)
+    return Response(serializer.data)

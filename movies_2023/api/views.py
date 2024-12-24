83eb7b19732af72225208bb384201e8db10fb8b6
Jx1126 <147470595+Jx1126@users.noreply.github.com>
2024-12-24 16:25:23 +0800

    Added pagination feature so data will split into multiple pages, reducing loading time

--- movies_2023/api/views.py
+++ movies_2023/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from . import pagination
 from rest_framework import status
 from .models import Movie, Genre, Director, Star
 from .serializers import MovieSerializer, GenreSerializer, DirectorSerializer, StarSerializer
@@ -7,8 +8,14 @@ from .serializers import MovieSerializer, GenreSerializer, DirectorSerializer, S
 @api_view(['GET'])
 def getAllMovies(request):
     movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True)
-    return Response(serializer.data)
+
+    # Paginate the movies
+    paginator = pagination.DataPagination()
+    paginated_movies = paginator.paginate_queryset(movies, request)
+
+    # Serialize the data and return the response
+    serializer = MovieSerializer(paginated_movies, many=True)
+    return paginator.get_paginated_response(serializer.data)
 
 # GET request to return movies by genre
 @api_view(['GET'])
@@ -18,10 +25,16 @@ def getMovieByGenre(request):
     # Capitalize the firt letter of the genre to match the database
     genres = [genre.capitalize() for genre in genres]
 
+    # Filter the movies by the genre
     movies = Movie.objects.filter(genre__name__in=genres)
+
+    # Paginate the movies
+    paginator = pagination.DataPagination()
+    paginated_movies = paginator.paginate_queryset(movies, request)
+
     # Serialize the data and return the response
-    serializer = MovieSerializer(movies, many=True)
-    return Response(serializer.data)
+    serializer = MovieSerializer(paginated_movies, many=True)
+    return paginator.get_paginated_response(serializer.data)
 
 # GET request to return movies by director or star
 @api_view(['GET'])
@@ -38,9 +51,13 @@ def getMovieByStarOrDirector(request):
     if director:
         movies = Movie.objects.filter(director__name__iexact=director)
 
+    # Paginate the movies
+    paginator = pagination.DataPagination()
+    paginated_movies = paginator.paginate_queryset(movies, request)
+
     # Serialize the data and return the response
     serializer = MovieSerializer(movies, many=True)
-    return Response(serializer.data)
+    return paginator.get_paginated_response(serializer.data)
 
 # GET request to return all genres
 @api_view(['GET'])

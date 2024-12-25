5fe0f6580d1bb3f3c7733c25150f873da02761d3
Jx1126 <147470595+Jx1126@users.noreply.github.com>
2024-12-25 18:36:23 +0800

    Reloaded database for the latest changes from the load_and_store.py

--- movies_2023/load_and_store.py
+++ movies_2023/load_and_store.py
@@ -10,54 +10,72 @@ from api.models import Genre, Director, Star, Movie
 csv_path = '../csv/2023_movies.csv'
 
 def loadAndStoreData():
-  try:
-    with open(csv_path, 'r',encoding='utf-8-sig') as file:
-      reader = csv.DictReader(file)
+    try:
+        # Open the CSV file and read the data
+        with open(csv_path, 'r',encoding='utf-8-sig') as file:
+            reader = csv.DictReader(file)
 
-      for row in reader:
+            print('Loading data...')
 
-        release_date = row['date'] if row['date'] else None
-        run_time = row['run_time'] if row['run_time'] else 0
-        rating = row['rating'] if row['rating'] else None
-        introduction = row['introduction'] if row['introduction'] else 'No Data'
+            for row in reader:
+                # Get the movie data from the CSV file and set the default values if the data is not available
+                release_date = row['date'] if row['date'] else None
+                run_time = row['run_time'] if row['run_time'] else 0
+                rating = row['rating'] if row['rating'] else None
+                introduction = row['introduction'] if row['introduction'] else 'No Data'
 
-        movie, created = Movie.objects.get_or_create(
-        title = row['title'].strip(),
-        defaults = {'release_date': release_date, 'run_time': run_time, 'rating': rating, 'introduction': introduction})
+                # Create a new movie object
+                movie, created = Movie.objects.get_or_create(
+                    # Get the title and remove whitespaces
+                    title = row['title'].strip(),
+                    # Set the default values for the movie object
+                    defaults = {'release_date': release_date, 'run_time': run_time, 'rating': rating, 'introduction': introduction}
+                )
 
-        genres = row['genre'].split(',')
-        genre_objects = []
-        for genre in genres:
-          genre_object, _ = Genre.objects.get_or_create(name=genre.strip())
-          genre_objects.append(genre_object)
-          movie.genre.set(genre_objects)
+                # Get the genres and split them by comma
+                genres = row['genre'].split(',')
+                genre_objects = []
+                for genre in genres:
+                    # Get or create the genre object depending on whether it exists or not
+                    genre_object, _ = Genre.objects.get_or_create(name=genre.strip())
+                    # Add the genre object to the genre_objects
+                    genre_objects.append(genre_object)
+                # Set the genres for the movie
+                movie.genre.set(genre_objects)
 
-        directors = row['director'].split(',')
-        director_objects = []
-        for director in directors:
-          director_object, _ = Director.objects.get_or_create(name=director.strip())
-          director_objects.append(director_object)
-          movie.director.set(director_objects)
+                # Get the directors and split them by comma
+                directors = row['director'].split(',')
+                director_objects = []
+                for director in directors:
+                    # Get or create the director object depending on whether it exists or not
+                    director_object, _ = Director.objects.get_or_create(name=director.strip())
+                    # Add the director object to the director_objects
+                    director_objects.append(director_object)
+                # Set the directors for the movie
+                movie.director.set(director_objects)
 
-        stars = row['stars'].split(',')
-        star_objects = []
-        for star in stars:
-          star_object, _ = Star.objects.get_or_create(name=star.strip())
-          star_objects.append(star_object)
-          movie.stars.set(star_objects)
+                stars = row['stars'].split(',')
+                star_objects = []
+                for star in stars:
+                    # Get or create the star object depending on whether it exists or not
+                    star_object, _ = Star.objects.get_or_create(name=star.strip())
+                    # Add the star object to the star_objects
+                    star_objects.append(star_object)
+                # Set the stars for the movie
+                movie.stars.set(star_objects)
 
-          movie.save()
+                # Save the movie object
+                movie.save()
 
-        print(f'{movie.title} added to the database')
-  
-  except FileNotFoundError:
-    print('File not found')
-  except Exception as e:
-    print(e)
+    # Return error messages
+    except FileNotFoundError:
+        print('File not found')
+    except Exception as e:
+        print(e)
 
 # Call the function
 def main():
-  loadAndStoreData()
-  print('Data loaded successfully')
+    loadAndStoreData()
+    print('Data loaded successfully')
 
 main()

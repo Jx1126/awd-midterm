162f90ca29d1eeb314275c69b2255bbb0d3f93fe
Jx1126 <147470595+Jx1126@users.noreply.github.com>
2024-12-25 16:30:26 +0800

    Implemented test cases for Serializers

--- movies_2023/api/tests.py
+++ movies_2023/api/tests.py
@@ -1,6 +1,40 @@
 from rest_framework.test import APITestCase
 from rest_framework import status
 from .models import *
+from .serializers import *
+
+class SerializersTestCases(APITestCase):
+    # Set up the movie data
+    def setUp(self):
+        self.genre = Genre.objects.create(name='Scifi')
+        self.director = Director.objects.create(name='Christopher Nolan')
+        self.star = Star.objects.create(name='Matthew McConaughey')
+        self.movie = Movie.objects.create(title='Interstellar', release_date='2023-11-26', run_time=169, rating=8.7, introduction='When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.')
+
+        # Add the genre, director, and star to the movie
+        self.movie.genre.add(self.genre)
+        self.movie.director.add(self.director)
+        self.movie.stars.add(self.star)
+
+    # Test the Genre serializer
+    def testGenreSerializer(self):
+        serializer = GenreSerializer(self.genre)
+        self.assertEqual(serializer.data, {'id': 1, 'name': 'Scifi'})
+
+    # Test the Director serializer
+    def testDirectorSerializer(self):
+        serializer = DirectorSerializer(self.director)
+        self.assertEqual(serializer.data, {'id': 1, 'name': 'Christopher Nolan'})
+
+    # Test the Star serializer
+    def testStarSerializer(self):
+        serializer = StarSerializer(self.star)
+        self.assertEqual(serializer.data, {'id': 1, 'name': 'Matthew McConaughey'})
+
+    # Test the Movie serializer
+    def testMovieSerializer(self):
+        serializer = MovieSerializer(self.movie)
+        self.assertEqual(serializer.data, {'id': 1, 'title': 'Interstellar', 'release_date': '2023-11-26', 'run_time': 169, 'rating': '8.7', 'introduction': 'When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.', 'genre': [{'id': 1, 'name': 'Scifi'}], 'director': [{'id': 1, 'name': 'Christopher Nolan'}], 'stars': [{'id': 1, 'name': 'Matthew McConaughey'}]})
 
 class ViewsTestCases(APITestCase):
   

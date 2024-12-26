c823b0b303ff58afd99584b6eadfc0639cee4ba7
Jx1126 <147470595+Jx1126@users.noreply.github.com>
2024-12-26 13:27:07 +0800

    Created admin page with all of the models.

--- movies_2023/api/admin.py
+++ movies_2023/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(Movie)
+admin.site.register(Director)
+admin.site.register(Star)
+admin.site.register(Genre)
